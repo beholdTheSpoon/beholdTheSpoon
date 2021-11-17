4226acf4c3be092f9d7784681995c2937e544e14
nnieminen <nnieminen@H29-14.insmollerussa.edu>
2021-11-17 14:35:01 +0100

    part 1 done

--- M3/src/Practica3ElPenjatBasica_nnieminen.java
+++ M3/src/Practica3ElPenjatBasica_nnieminen.java
@@ -7,50 +7,65 @@ public class Practica3ElPenjatBasica_nnieminen {
 		Scanner scanner = new Scanner(System.in);
 		String paraulaSecreta = "practicajava";
 		char paraulaJoc[] = new char[paraulaSecreta.length()];
-		//boolean ple[] = new boolean[paraulaSecreta.length()];
+		boolean perdut = false;
+		int c = 0;
+		int cL = 0;
 		
 		int intents = 10;
 
-		for (int i = 0; i < paraulaJoc.length; i++) {
+		for(int i = 0; i < paraulaJoc.length; i++) {
 			paraulaJoc[i] = '-';
 		}
 		
-		for (int i = 0; i < intents; i++) {
-			
-			if(i == intents-1) {
-				System.out.println("Has perdut.");
-				break;
+		System.out.println("Joc del penjat.");
+
+		for(int i = 0; i < intents; i++) { //bucle principal
+			cL = 0;
+			c = 0;
+			if(perdut == false) {
+				System.out.println("\nCada intent equivocat resta un intent. Intents que queden: " + (intents-i) + "\nIntrodueix un caracter: ");
 			}
 			
-			System.out.println("\nCada intent equivocat resta un intent. Intents que queden: " + (intents-i) + "\nIntrodueix un caracter: ");
-			
-			
-			for (int l = 0; l < paraulaJoc.length; l++) {
+			for(int l = 0; l < paraulaJoc.length; l++) {
 				System.out.print(paraulaJoc[l]);
 			}
 			
 			System.out.println();
-			
 			String resposta = scanner.next();
 			char r = resposta.charAt(0);
-			int c = 0;
-
-			for (int j = 0; j < paraulaJoc.length; j++) {
+			
+			for(int j = 0; j < paraulaJoc.length; j++) {
+				if(paraulaJoc[j] == '-') {
+					cL++;
+				}
+				
 				if(paraulaSecreta.charAt(j) == r) {
 					paraulaJoc[j] = r;
 					c++;
 				}
 			}
 			
+			if(i == intents-1) {
+				perdut = true;
+				System.out.println("Has perdut.");
+				System.out.print("La paraula era: " + paraulaSecreta);
+				break;
+			} 
+			
+			if(cL == 1 && c>0) {
+				System.out.println("Has guanyat!");
+				System.out.print("La paraula era: " + paraulaSecreta );
+				break;
+			}
+			
 			if(c == 0) {
 				System.out.println("Malament.");
 			} else if(c > 0) {
-				i--;
+				intents++;
 			}
 
 			System.out.println();
 		}
 		scanner.close();
 	}
-	
 }
