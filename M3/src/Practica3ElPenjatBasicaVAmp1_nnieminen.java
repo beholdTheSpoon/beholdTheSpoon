b5957e643c93a522b3e050ea3eba6f6eca8c99c6
Propietario <Propietario@bruhmoment>
2021-11-21 11:52:13 +0100

    yeet

--- M3/src/Practica3ElPenjatBasicaVAmp1_nnieminen.java
+++ M3/src/Practica3ElPenjatBasicaVAmp1_nnieminen.java
@@ -1,11 +1,44 @@
 import java.util.Scanner;
 
-public class Practica3ElPenjatBasica_nnieminen {
+public class Practica3ElPenjatBasicaVAmp1_nnieminen {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		String paraulaSecreta = "practicajava";
 		char paraulaJoc[] = new char[paraulaSecreta.length()];
+		char ninot[][] = new char[7][5];
+	
+		//fa pena fer-la tan manualment perque el ninot es una forma complicat.
+		int pal[][] = {		{0,1},
+							{0,2},
+							{0,3}};
+		
+		int cos[][] = {		{1,3},
+							{2,3},
+							{3,3}};
+		
+		int esquerra[][] = {{3,2},
+							{4,2}};
+		
+		int dreta[][] = {	{4,4},
+							{3,4}};
+		
+		char parts[] = {'-','|','/','O','\\'};
+
+		//int cN = passos.length-(passos.length-1);
+		
+		for (int i = 0; i < ninot.length; i++) {
+			for (int j = 0; j < ninot[i].length; j++) {
+				if(i > 0 && i < ninot.length && j==0) {
+					ninot[i][j] = '|';
+				}
+				
+				if(i==ninot.length-1 && j>=0 && j<ninot[j].length) {
+					ninot[i][j] = '=';
+				}
+			}
+		}
+		
 		boolean perdut = false;
 		int c = 0;
 		int cL = 0;
@@ -25,9 +58,19 @@ public class Practica3ElPenjatBasica_nnieminen {
 				System.out.println("\nCada intent equivocat resta un intent. Intents que queden: " + (intents-i) + "\nIntrodueix un caracter: ");
 			}
 			
+			
 			for(int l = 0; l < paraulaJoc.length; l++) {
 				System.out.print(paraulaJoc[l]);
 			}
+			System.out.println();
+			for (int j = 0; j < ninot.length; j++) {
+				for (int k = 0; k < ninot[j].length; k++) {
+					System.out.print(j);
+					System.out.print(k);
+					System.out.print(ninot[j][k]);
+				}
+				System.out.println();
+			} 
 			
 			System.out.println();
 			String resposta = scanner.next();
@@ -59,6 +102,14 @@ public class Practica3ElPenjatBasica_nnieminen {
 			
 			if(c == 0) {					//CONDICIO DE CONTINUACIO/PERDUA DE INTENTS: si no es troba cap caracter que coincideix amb la resposta es continua normalment, pero si es troba una coincidencia, s'incrementa intents perque el bucle continui mes.
 				System.out.println("Malament.");
+				for (int j = 0; j < cN; j++) {
+					for (int k = 0; k < passos[j].length; k++) {
+						if()
+								//ninot[j][k] = 
+					}
+				}
+				
+			cN++;
 			} else if(c > 0) {
 				intents++;
 			}
