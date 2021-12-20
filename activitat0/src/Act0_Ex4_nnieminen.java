0ab0099a48e48c12747dd7ad99d35264e9ad68ca
Propietario <Propietario@bruhmoment>
2021-12-20 18:45:36 +0100

    done

--- activitat0/src/Act0_Ex4_nnieminen.java
+++ activitat0/src/Act0_Ex4_nnieminen.java
@@ -1,40 +1,28 @@
+
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class Act0_Ex4_nnieminen {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String frase = scanner.nextLine();
+		System.out.println("Introdueix un frase.");
 		String paraulaProhibida = "hola";
-		paraulaProhibida = Pattern.quote(paraulaProhibida);
-		StringBuilder sfrase = new StringBuilder(frase);
-		String fraseNou = null;
+		String frase = scanner.nextLine();
+		String subs[] = frase.split(" ");
+		
 		scanner.close();
 		
-		Pattern paraula = Pattern.compile(paraulaProhibida, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = paraula.matcher(frase);
-		
-		while (matcher.find()) {
-		     System.out.println(matcher.start() +" - "+ (matcher.end()-1));
-		     for(int i = 0; i<frase.length(); i++) {
-		    	 if(i>=matcher.start() && i<=matcher.end()) {
-		    		sfrase.insert(i, "X");		    	 }
-		     }
+		for(int i = 0; i<subs.length; i++) {
+			if(subs[i].equals(paraulaProhibida)) {
+				for(int j = 0; j<subs[i].length(); j++) {
+					subs[i] = subs[i].replace(subs[i].charAt(j), 'X');
+				}
+			}
 		}
-		
-		System.out.println(matcher);
-		System.out.println(sfrase);
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		for(String i : subs) {
+			System.out.print(i+" ");
+		}
 	}
 
 }
