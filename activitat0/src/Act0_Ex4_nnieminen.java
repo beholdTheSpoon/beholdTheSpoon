29719f13d8e37fc51c24c60dae25e0c6de8a8dde
nnieminen <nnieminen@H29-14.insmollerussa.edu>
2021-12-20 09:55:16 +0100

    tes

---
+++ activitat0/src/Act0_Ex4_nnieminen.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Act0_Ex4_nnieminen {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String frase = scanner.nextLine();
+		scanner.close();
+		
+		
+	}
+
+}
