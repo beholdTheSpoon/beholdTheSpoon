b8246e568c45ec4c0a5e8ca14d128875aeea0ba1
Propietario <Propietario@bruhmoment>
2022-01-16 16:00:22 +0100

    amongus

---
+++ funcions/Exercici10.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Exercici10 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean continuar = true;
+		
+		while (continuar == true) {
+			System.out.println("Introdueix 2 numeros enters: ");
+			int n1 = scanner.nextInt();
+			int n2 = scanner.nextInt();
+			
+			System.out.println("1. Sumar " + n1 + "+" + n2);
+			System.out.println("2. Restar " + n1 + "-" + n2);
+			System.out.println("3. Multiplicar " + n1 + "*" + n2);
+			System.out.println("4. Dividir " + n1 + "/" + n2);
+			System.out.println("Elegeix introduint el numero de la opcio");
+			
+			int opcio = scanner.nextInt();
+				switch (opcio) {
+					case 1: 
+						System.out.println(suma(n1, n2));
+						break;
+					
+					case 2: 
+						System.out.println(resta(n1, n2));
+						break;
+					
+					case 3: 
+						System.out.println(multiplicar(n1, n2));
+						break;
+					
+					case 4: 
+						System.out.println(divisio(n1, n2));
+
+						break;
+					
+					default:
+						System.out.println("Invalid");
+				}
+
+			System.out.println("Vols continuar? (S/N)");
+			String resposta = scanner.next();
+			if(resposta.equals("S")) {
+				continuar = true;
+				System.out.println("Continuant...");
+			} else if(resposta.equals("N")) {
+				continuar = false;
+				System.out.println("Parant...");
+				break;
+			}
+		}
+
+	}
+	
+	static int suma(int x, int y) {
+		return x+y;
+	}
+	
+	static int resta(int x, int y) {
+		return x-y;
+	}
+	
+	static int multiplicar(int x, int y) {
+		return x*y;
+	}
+	
+	static int divisio(int x, int y) {
+		return x/y;
+	}
+	
+}
