a0bbad2df1ba018d59787bd851c4598b31ce73d2
Propietario <Propietario@bruhmoment>
2022-02-07 12:39:57 +0100

    ghjgh

--- recursivitat/src/Problema2.java
+++ recursivitat/src/Problema2.java
@@ -1,31 +1,44 @@
 
-public class Problema2 {
+public class problema2 {
 
 	public static void main(String[] args) {
-		
-		int[] array = {3, 1, 5, 6, 3, 8, 3, 5, 7, 4, 3, 0};
+
+		problema2 programa = new problema2();
+
+		programa.inici();
+
+	}
+
+	public void inici() {
+		int[] array = { 2, 2, 8, 3, 1, 3, 8, 4, 3, 9, 3, 0, 1, 6, 4, 9, 3 };
 		int valor = 3;
 		int posInici = 0;
-		
+
 		int resultat = comptar(array, valor, posInici);
-		System.out.println(resultat);
+		System.out.println("El numero " + valor + " apareix " +resultat+ " vegades en el array.");
 	}
-	
+
 	public static int comptar(int[] array, int valor, int posInici) {
-		int n = 0;
-		int c = posInici;
-		
-		if(array[c] != valor) {
-			if(c<array.length-1 && c<array.length) {
-			c++;
-			return comptar(array, valor, c);
+		if (posInici == array.length) {
+			
+			return 0;
+			
+		} else {
+
+			if (array[posInici] != valor && posInici < array.length) {
+
+				return comptar(array, valor, posInici + 1);
+
 			}
-		} else if(array[c] == valor && c<array.length) {
-			if(c<array.length-1) {
-			n++;
-			c++;
-			return comptar(array, valor, c);
-		}}
-		return n;
+			if (array[posInici] == valor && posInici < array.length) {
+
+			
+
+				return comptar(array, valor, posInici + 1) + 1;
+
+			}
+			return 0;
+		}
 	}
+
 }
