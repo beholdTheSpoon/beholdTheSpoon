0a032a0e4b36cf35ad648d4659aa63b633b8b83e
nnieminen <nnieminen@H29-14.insmollerussa.edu>
2022-02-28 09:50:14 +0100

    amogus

---
+++ UF3/src/UF3Practica1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class UF3Practica1 {
+
+	public static void main(String[] args) {
+		UF3Practica1 program = new UF3Practica1();
+		program.inici();
+	}
+	
+	public void inici() {
+		
+	}
+	
+	public int comptarParaulesLinia() {
+		
+	}
+	
+	public int comptarParaulesTotal() {
+		
+	}
+	
+	public int comptarParaula() {
+		
+	}
+
+}
